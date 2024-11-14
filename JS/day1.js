5c49e2d5add2ccf4c55f7d80e8ac9c5319ef3615
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-11-14 22:15:20 +0500

    DAY 2 JS

--- JS/day1.js
+++ JS/day1.js
@@ -1,9 +1,67 @@
-console.log("Hello Zeeshan");
+const num1 = 10;
+const num2 = 20;
+console.log(num1 * num2, "yeh num1 aur num2 ka product hai")
 
-//PROGRAM TO ADD TWO NUMBERS
 
-const numberOne = "Areeb";   0
 
-//STRING
+const number1 = 20;
+const number2 = 30;
+console.log(number1 + number2, "YEH 20 + 30 ka sum hai") //50
 
-const numberTwo = "Sadaqat";
+
+
+//LOGICAL OPERATORS
+// AND OPERATOR - &&
+// OR OPERATOR - ||
+// NOT OPERATOR - !
+
+
+// AND OPERATOR
+
+console.log(true && true, "AND OPERATOR")
+
+
+// OR OPERATOR
+
+console.log(typeof (false || false), "OR OPERATOR")
+
+
+// NOT OPERATOR
+
+console.log(!false, "NOT OPERATOR")
+
+
+var newVarible = null
+console.log(typeof (newVarible), "newVarible")
+
+
+
+
+
+
+
+
+
+
+
+//LOOPS
+
+//FOR LOOP
+//WHILE LOOP
+//DO WHILE LOOP
+
+
+
+
+
+var tableNum = 67;
+
+for(var i = 1; i <= 10; i++) {
+    console.log(tableNum + "*" + i + "=" + tableNum * i)   
+}
+
+
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
